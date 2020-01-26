08767ef2c233ab5ca519cea49bac5116f3744248
Andrew Kampjes <a.kampjes@gmail.com>
2020-01-26 16:14:04 +1300

    Adjust cap to apply rbenv on even more binaries

--- Capfile
+++ Capfile
@@ -18,8 +18,8 @@ install_plugin Capistrano::SCM::Git
 require 'capistrano/rails'
 require "capistrano/rails/assets"
 require "capistrano/rails/migrations"
-require 'capistrano/bundler'
 require 'capistrano/rbenv'
+require 'capistrano/bundler'
 require 'capistrano/puma'
 install_plugin Capistrano::Puma  # Default puma tasks
 install_plugin Capistrano::Puma::Nginx  # if you want to upload a nginx site template
--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,8 @@ set :bundle_without, %w{development test}.join(' ')
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
+# append :rbenv_map_bins, 'puma', 'pumactl' # Alternatively we may extend the binmaps
 set :rbenv_roles, :all # default value
 
 # Default branch is :master
