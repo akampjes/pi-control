cb3d80436405f8cf807908d5249bced190d3a244
Andrew Kampjes <a.kampjes@gmail.com>
2020-02-08 14:16:16 +1300

    Add base sensor devices seeds

---
+++ spec/models/temperature_sensor_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe TemperatureSensor do
+  subject(:temperature_sensor) { TemperatureSensor.create(name: 'temp sensor 1') }
+
+  it 'has basic fields and is valid' do   #
+    expect(temperature_sensor).to be_valid
+    expect(temperature_sensor.name).to eql 'temp sensor 1'
+    expect(temperature_sensor.configuration).to be_empty
+  end
+end
