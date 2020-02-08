98092c41e759e7392b5383a1efa05aa8adac22b7
Andrew Kampjes <a.kampjes@gmail.com>
2020-02-09 12:23:59 +1300

    Frist cd to directory before running job

--- scripts/cornjob.sh
+++ scripts/cornjob.sh
@@ -5,4 +5,5 @@ export RAILS_ENV=production
 export RBENV_ROOT=$HOME/.rbenv
 export RBENV_VERSION=2.6.5
 
+cd /home/pi/apps/pi-control/current
 $HOME/.rbenv/bin/rbenv exec bundle exec rake job:read_temperatures
